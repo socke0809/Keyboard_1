a432fda5bf35f9a4b23d9f28466a2a0c07dab002
socke0809 <steffi@dangl-muenchen.de>
2013-08-22 00:15:43 +0200

    prueft startbit

--- main.c
+++ main.c
@@ -4,14 +4,21 @@
 
 enum ps2ReceiveState    state       = start;
 volatile uint8_t        ps2FlagRcv  = 0;
+volatile uint8_t 		ps2RcvDataByte;
+
+//PD2- Pin 2 am arduino - clk
+//PD1- pin 1 am arduino - data 
+// in PORTB sollen die gesendeten bit rein 
 
 
 ISR( INT0_vect )
 {
 	switch(state){
 		case start:
-			//TODO prüfen ob 0 übertragen wurde
-			state =	data;
+			if(!(PIND & (1<<PD1))){ // überprüft startbit = 0 
+				state 			=	data;
+				ps2RcvDataByte	=	0;
+			}
 			break;
 
 		case parity:
@@ -42,7 +49,7 @@ int main()
 
 
     while(1){
-        if( 1 ){ // TODO: check if transfer complete and no error occured
+        if(ps2FlagRcv){ // TODO: check if transfer complete and no error occured
             //TODO Translate
         }
     }
