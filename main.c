e7519eaaa9cbbaa1bda338e0e445d9322f48b951
socke0809 <steffi@dangl-muenchen.de>
2013-08-22 01:33:05 +0200

    Added ParityCheck and Stop

--- main.c
+++ main.c
@@ -1,41 +1,62 @@
 #include <avr/interrupt.h>
 #include "main.h"
 
+#define PS2_DATA		PD1
+#define PS2_DATA_PIN	PIND
+#define PS2_DATA_DDR	DDRD
+#define PS2_DATA_PORT	PORTD
+
+#define PS2_CLK			PD2
+#define PS2_CLK_PIN		PIND
+#define PS2_CLK_DDR		DDRD
+#define PS2_CLK_PORT	PORTD
+
+
 
 enum ps2ReceiveState    state       = start;
 volatile uint8_t        ps2FlagRcv  = 0;
 volatile uint8_t 		ps2RcvDataByte;
+volatile uint8_t		ps2ParityControl = 0;
 
-//PD2- Pin 2 am arduino - clk
-//PD1- pin 1 am arduino - data 
-// in PORTB sollen die gesendeten bit rein 
+
 
 
 ISR( INT0_vect )
 {
+	int loopCount = 8;
 	switch(state){
 		case start:
-			if(!(PIND & (1<<PD1))){ // überprüft startbit = 0 
+			if(!(PS2_DATA_PIN & (1<<PS2_DATA))){ // überprüft startbit = 0 
 				state 			=	data;
 				ps2RcvDataByte	=	0;
 			}
 			break;
 
 		case parity:
-			//TODO parity check
-				//TODO: wenn parity ist falsch
-                //TODO: setze error flag
-			state =	stop;
+			
+			while(loopCount--){
+			
+				ps2ParityControl ^= ps2RcvDataByte&1;
+				ps2RcvDataByte <<= 1;
+			}
+			if(PS2_DATA != ps2ParityControl){
+				ps2FlagRcv	=	PS2_FLAG_RCV_ERROR ;
+			}
+			else{
+				state =	stop;
+			}
 			break;
 
 		case stop:
-            //TODO: prüfen ob 1 übertragen wurde
-            //TODO: setze complete flag
-			state = start;
+            if(PS2_DATA_PIN & (1<<PS2_DATA)){	//prüft stopbit = 1
+				ps2FlagRcv		=	PS2_FLAG_RCV_COMPLETE;
+				state = start;
+			}
 			break;
             
 		default:
 			//TODO: read data
+			
 			state++;
 			break;
 
