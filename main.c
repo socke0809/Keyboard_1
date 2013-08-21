19f4814e5a8d8546ee116471da20e2ed24b183dd
socke0809 <steffi@dangl-muenchen.de>
2013-08-21 03:13:45 +0200

    "started IRS"

--- main.c
+++ main.c
@@ -5,11 +5,27 @@ enum ps2ReceiveState = {
 	parity  =   9, 
 	stop    =   10
 };
+char clk;
+char data;
 
 
 ISR( INT0_vect )
 {
-}
+	if(clk == 0){
+		if((ps2ReceiveState > 0) && (ps2ReceiveState < 9)){ 
+			//TODO: read data
+			ps2ReceiveState++;
+			}
+		else if(Ps2ReceiveState == 9){			//parity bit
+			//TODO: parity bit 
+			ps2ReceiveState++;
+			}
+		else{									//stop bit
+			ps2ReceiveState = 0;
+			}
+		}
+	
+	
 
 
 int main()
