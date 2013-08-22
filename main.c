a0f7a3a383ea737463a195dc4e337de6c6f8fa9f
socke0809 <steffi@dangl-muenchen.de>
2013-08-22 02:57:37 +0200

    fixed defines

--- main.c
+++ main.c
@@ -1,22 +1,14 @@
 #include <avr/interrupt.h>
 #include "main.h"
 
-#define PS2_DATA		PD1
-#define PS2_DATA_PIN	PIND
-#define PS2_DATA_DDR	DDRD
-#define PS2_DATA_PORT	PORTD
 
-#define PS2_CLK			PD2
-#define PS2_CLK_PIN		PIND
-#define PS2_CLK_DDR		DDRD
-#define PS2_CLK_PORT	PORTD
 
 
 
 enum ps2ReceiveState    state       = start;
-volatile uint8_t        ps2FlagRcv  = 0;
-volatile uint8_t 		ps2RcvDataByte;
-volatile uint8_t		ps2ParityControl = 0;
+volatile uint8_t	ps2RcvFlag  = 0;
+volatile uint8_t 	ps2RcvDataByte;
+
 
 
 
@@ -24,10 +16,11 @@ volatile uint8_t		ps2ParityControl = 0;
 ISR( INT0_vect )
 {
 	int loopCount = 8;
+	int ps2ParityControl = 0;
 	switch(state){
 		case start:
-			if(!(PS2_DATA_PIN & (1<<PS2_DATA))){ // überprüft startbit = 0 
-				state 			=	data;
+			if(!(PS2_DATA_PIN & (1<<PS2_DATA))){	// überprüft startbit = 0 
+				state 		=	data;
 				ps2RcvDataByte	=	0;
 			}
 			break;
@@ -35,22 +28,19 @@ ISR( INT0_vect )
 		case parity:
 			
 			while(loopCount--){
-			
-				ps2ParityControl ^= ps2RcvDataByte&1;
-				ps2RcvDataByte <<= 1;
+				ps2ParityControl	^= 	ps2RcvDataByte&1;
+				ps2RcvDataByte		<<=	1;
 			}
 			if(PS2_DATA != ps2ParityControl){
-				ps2FlagRcv	=	PS2_FLAG_RCV_ERROR ;
+				ps2RcvFlag	|=	PS2_RCV_FLAG_ERROR ;
 			}
-			else{
-				state =	stop;
-			}
+				state		=	stop;
 			break;
 
 		case stop:
-            if(PS2_DATA_PIN & (1<<PS2_DATA)){	//prüft stopbit = 1
-				ps2FlagRcv		=	PS2_FLAG_RCV_COMPLETE;
-				state = start;
+            if(PS2_DATA_PIN & (1<<PS2_DATA)){	// prüft stopbit = 1
+				ps2RcvFlag	|=	PS2_RCV_FLAG_COMPLETE;
+				state		 =	start;
 			}
 			break;
             
@@ -70,7 +60,7 @@ int main()
 
 
     while(1){
-        if(ps2FlagRcv){ // TODO: check if transfer complete and no error occured
+        if(ps2RcvFlag){ // TODO: check if transfer complete and no error occured
             //TODO Translate
         }
     }
--- main.h
+++ main.h
@@ -1,9 +1,19 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
-#define PS2_FLAG_RCV_COMPLETE   (1<<0)
-#define PS2_FLAG_RCV_RECEIVING  (1<<1)
-#define PS2_FLAG_RCV_ERROR      (1<<2)
+#define PS2_RCV_FLAG_COMPLETE	(1<<0)
+#define PS2_RCV_FLAG_RECEIVING	(1<<1)
+#define PS2_RCV_FLAG_ERROR	(1<<2)
+
+#define PS2_DATA	PD1
+#define PS2_DATA_PIN	PIND
+#define PS2_DATA_DDR	DDRD
+#define PS2_DATA_PORT	PORTD
+
+#define PS2_CLK		PD2
+#define PS2_CLK_PIN		PIND
+#define PS2_CLK_DDR		DDRD
+#define PS2_CLK_PORT	PORTD
 
 enum ps2ReceiveState {
 	start   =   0,
