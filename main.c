534b94131e3f9df1c63efd94d7da11c2c6ece88d
socke0809 <steffi@dangl-muenchen.de>
2013-08-22 19:53:19 +0200

    datenlesen

--- main.c
+++ main.c
@@ -6,8 +6,8 @@
 
 
 enum ps2ReceiveState    state       = start;
-volatile uint8_t	ps2RcvFlag  = 0;
-volatile uint8_t 	ps2RcvDataByte;
+volatile uint8_t        ps2RcvFlag  = 0;
+volatile uint8_t        ps2RcvDataByte;
 
 
 
@@ -17,6 +17,7 @@ ISR( INT0_vect )
 {
 	int loopCount = 8;
 	int ps2ParityControl = 0;
+	int bitCount = 0;
 	switch(state){
 		case start:
 			if(!(PS2_DATA_PIN & (1<<PS2_DATA))){	// überprüft startbit = 0 
@@ -34,20 +35,26 @@ ISR( INT0_vect )
 			if(PS2_DATA != ps2ParityControl){
 				ps2RcvFlag	|=	PS2_RCV_FLAG_ERROR ;
 			}
-				state		=	stop;
+				state	=	stop;
 			break;
 
 		case stop:
             if(PS2_DATA_PIN & (1<<PS2_DATA)){	// prüft stopbit = 1
-				ps2RcvFlag	|=	PS2_RCV_FLAG_COMPLETE;
-				state		 =	start;
+					ps2RcvFlag	|=	PS2_RCV_FLAG_COMPLETE;
+					state        =	start;
 			}
 			break;
             
 		default:
-			//TODO: read data
-			
+			if(PS2_DATA_PIN & (1<<PS2_DATA)){ 
+				ps2RcvDataByte |= (1<<bitCount);
+				}
+			else{
+				ps2RcvDataByte &= ~(1<<bitCount);
+				}
+			bitCount++;
 			state++;
+			ps2RcvFlag  |=	PS2_RCV_FLAG_RECEIVING;
 			break;
 
 	}
@@ -60,7 +67,7 @@ int main()
 
 
     while(1){
-        if(ps2RcvFlag){ // TODO: check if transfer complete and no error occured
+        if(ps2RcvFlag){ // TODO: check if transfer complete and no error occured 
             //TODO Translate
         }
     }
