adfa6b2af3af0e5195b7608c3b8de07da3f0d498
socke0809 <steffi@dangl-muenchen.de>
2013-08-25 02:46:17 +0200

    Fehler behoben

--- main.c
+++ main.c
@@ -12,20 +12,32 @@ volatile uint8_t	ps2DataByte;
 
 
 
-void ps2_send_byte(){
+void ps2_send_byte(uint8_t x){
 	ps2Flag 	= PS2_FLAG_SENDING;
+	ps2DataByte = x;
 	PS2_CLK_DDR |= (1<<PS2_CLK); //sets clk as output 
 	PS2_CLK_PORT &= ~(1<<PS2_CLK); //sets clk low
 	_delay_us( 100 );
 	PS2_DATA_DDR |= (1<<PS2_DATA); //sets data output
 	PS2_DATA_PORT &= ~(1<<PS2_DATA);
 	PS2_CLK_DDR &= ~(1<<PS2_CLK);
+	
 }
 
-void ps2_receive_byte(uint8_t *x){
+int8_t ps2_receive_byte(uint8_t *x){
 	if(ps2Flag & PS2_FLAG_RCV_COMPLETE){
+		if(!(ps2Flag & PS2_FLAG_ERROR)){
 			(*x) = ps2DataByte;
+			return 0;
 		}
+		else{
+			return -2;
+		}
+	}
+	else{
+		return -1;
+	}
+		
 }
 
 int parity_control(uint8_t x){
@@ -38,9 +50,11 @@ int parity_control(uint8_t x){
 	return ps2ParityControl;
 }
 
-void ps2_write_data(uint8_t x){
-	ps2DataByte = x;
+uint8_t getState(){
+	return ps2Flag;
 }
+
+
 
 	
 	
@@ -92,12 +106,6 @@ ISR( INT0_vect )
 	}
 	else{
 		switch(state){
-			case start:
-				if(!(PS2_DATA_PIN & (1<<PS2_DATA))){	// überprüft startbit = 0 
-					state 		=	data;
-				}
-				break;	
-				
 			case parity:
 				if(PS2_DATA != parity_control(ps2DataByte)){
 					ps2Flag	|=	PS2_FLAG_ERROR ;
@@ -112,18 +120,21 @@ ISR( INT0_vect )
 				break;
 				
 			case acknowledge:
+			
 				if(PS2_DATA_PIN &(1<<PS2_DATA)){
 					ps2Flag	|=	PS2_FLAG_ERROR; 
 				}
 				else{
 				ps2Flag  |= PS2_FLAG_TRANSF_COMPLETE;
+				
 				}
+				ps2Flag	&=	~(PS2_FLAG_SENDING);
 				state = start;
 				break;
 			
-			default:
-				ps2DataByte >>= 1;
-				if(!ps2DataByte){
+			case data:
+			
+				if(ps2DataByte&1){
 					PS2_DATA_PORT &= ~(1<<PS2_DATA);
 				}
 				else{
@@ -131,6 +142,7 @@ ISR( INT0_vect )
 				}
 				state++;
 				ps2Flag	|=	PS2_FLAG_SENDING; 
+				ps2DataByte >>= 1;
 				break;
 		}
 	}		
