bdaa00a63497595605dfcc95066c61b6e27b7710
x5444 <accounts@x5444.com>
2013-08-27 21:12:23 +0200

    Fixed a couple of names in PS2_HW lib.

--- ps2_hw.h
+++ ps2_hw.h
@@ -4,22 +4,22 @@
 #include <inttypes.h>
 
 
-#define PS2_FLAG_RCV_COMPLETE   	(1<<0)
-#define PS2_FLAG_TRANSF_COMPLETE 	(1<<1)
-#define PS2_FLAG_RECEIVING			(1<<2)
-#define PS2_FLAG_ERROR				(1<<3)
-#define PS2_FLAG_SENDING			(1<<4)
+#define PS2_HW_FLAG_RCV_COMPLETE       	(1<<0)
+#define PS2_HW_FLAG_TRANSF_COMPLETE 	(1<<1)
+#define PS2_HW_FLAG_RECEIVING			(1<<2)
+#define PS2_HW_FLAG_ERROR				(1<<3)
+#define PS2_HW_FLAG_SENDING	    		(1<<4)
 
 
-#define PS2_DATA        PD1
-#define PS2_DATA_PIN    PIND
-#define PS2_DATA_DDR    DDRD
-#define PS2_DATA_PORT   PORTD
+#define PS2_HW_DATA        PD1
+#define PS2_HW_DATA_PIN    PIND
+#define PS2_HW_DATA_DDR    DDRD
+#define PS2_HW_DATA_PORT   PORTD
 
-#define PS2_CLK         PD2
-#define PS2_CLK_PIN     PIND
-#define PS2_CLK_DDR     DDRD
-#define PS2_CLK_PORT    PORTD
+#define PS2_HW_CLK         PD2
+#define PS2_HW_CLK_PIN     PIND
+#define PS2_HW_CLK_DDR     DDRD
+#define PS2_HW_CLK_PORT    PORTD
 
 
 void ps2_hw_init( void );
